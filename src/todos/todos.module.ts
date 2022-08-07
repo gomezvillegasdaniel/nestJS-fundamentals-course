5a31321e0edf5705e3cb6676a49c24c215c60a32
Daniel Gomez <danielgomez@ibm.com>
2022-08-07 09:27:35 -0500

    creating a typeORM entity for Todo

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosController } from './controller/todos.controller';
+import { Todo } from './entity/todo.entity';
 import { TodosService } from './service/todos.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Todo])],
   controllers: [TodosController],
   providers: [TodosService],
 })
