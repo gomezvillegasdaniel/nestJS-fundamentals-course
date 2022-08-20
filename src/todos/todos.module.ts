22b6b456c60e8e0eca3f76bc6cba32ce9d45e9f9
Daniel Gomez <danielgomez@ibm.com>
2022-08-20 11:03:36 -0500

    modifying TODO_EXTRA_TAGS provider to be async

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,6 +1,7 @@
 import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
+import { Connection } from 'typeorm';
 import { TodosController } from './controller/todos.controller';
 import { Tag } from './entity/tag.entity';
 import { Todo } from './entity/todo.entity';
@@ -12,13 +13,13 @@ class DevConfigService {}
 class ProdConfigService {}
 // class MockTodosService {}
 
-@Injectable()
-export class TodosExtraTagsFactory {
-  create() {
-    // do something
-    return ['TAG4', 'TAG5', 'TAG6'];
-  }
-}
+// @Injectable()
+// export class TodosExtraTagsFactory {
+//   create() {
+//     // do something
+//     return ['TAG4', 'TAG5', 'TAG6'];
+//   }
+// }
 
 @Module({
   imports: [TypeOrmModule.forFeature([Todo, Tag, Event])],
@@ -26,7 +27,7 @@ export class TodosExtraTagsFactory {
   // providers: [{ provide: TodosService, useValue: new MockTodosService() }], // use this in case you need to use a mock as a provider
   providers: [
     TodosService,
-    TodosExtraTagsFactory,
+    // TodosExtraTagsFactory,
     {
       provide: ConfigService,
       useClass:
@@ -35,9 +36,14 @@ export class TodosExtraTagsFactory {
     // { provide: TODO_EXTRA_TAGS, useValue: ['TAG1', 'TAG2'] },
     {
       provide: TODO_EXTRA_TAGS,
-      useFactory: (todosExtraTagsFactory: TodosExtraTagsFactory) =>
-        todosExtraTagsFactory.create(),
-      inject: [TodosExtraTagsFactory],
+      // Note "async" here, and Promise/Async event inside the Factory function
+      // Could be a database connection / API call / etc
+      // In our case we're just "mocking" this type of event with a Promise
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        // const todoTags = await connection.query('SELECT * ...');
+        return Promise.resolve(['TAG7', 'TAG8']);
+      },
+      inject: [Connection],
     },
   ],
   exports: [TodosService],
