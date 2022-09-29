729845ecceed02af987afa86de165a5a1e8b7e15
Daniel Gomez <danielgomez@ibm.com>
2022-09-29 16:28:24 -0500

    swagger initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
@@ -18,6 +19,16 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TimeoutInterceptor());
+
+  // Swagger setup
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('ToDos')
+    .setDescription('This is a description for the ToDos app.')
+    .setVersion('1.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('swagger', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
