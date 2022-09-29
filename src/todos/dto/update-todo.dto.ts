992c3a32f277ff474cec834c71a8dad199b813c8
Daniel Gomez <danielgomez@ibm.com>
2022-09-29 16:43:09 -0500

    making swagger to show example values for update-todo-dto by importing PartialType from @nestjs/swagger

--- src/todos/dto/update-todo.dto.ts
+++ src/todos/dto/update-todo.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
+// import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger'; // this also implements @nestjs/mapped-types while enables the Dto for Swagger
 import { CreateTodoDto } from './create-todo.dto';
 
 // PartialType returns the type of the class is passed in it with the types declared as optional
