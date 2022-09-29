58050d3d34968aa82867a3ea79c037d3b521f3a4
Daniel Gomez <danielgomez@ibm.com>
2022-09-29 16:48:28 -0500

    adding explicit descriptions for swagger by using decorators on dtos

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateTodoDto {
+  @ApiProperty({ description: 'this is an explicit description' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty()
   @IsString()
   readonly description: string;
 
+  @ApiProperty()
   @IsString({ each: true })
   readonly tags: string[];
 }
