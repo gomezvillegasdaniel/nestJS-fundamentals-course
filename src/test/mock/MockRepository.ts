866267ed369be3db30f462fbae7148773d7a5a8d
Daniel Gomez <danielgomez@ibm.com>
2022-09-30 13:52:31 -0500

    exporting MockRepository

--- src/test/mock/MockRepository.ts
+++ src/test/mock/MockRepository.ts
@@ -1,6 +1,7 @@
 import { Repository } from 'typeorm';
 
-type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+// eslint-disable-next-line prettier/prettier
+export type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 export const createMockRepository = <T = any>(): MockRepository<T> => ({
   findOne: jest.fn(),
