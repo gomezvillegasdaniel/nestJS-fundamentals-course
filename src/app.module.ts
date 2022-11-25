196ddbe2c4692a81c5c9897e837aa05b323b5f3c
Daniel Gomez <danielgomez@ibm.com>
2022-11-25 16:19:35 -0600

    importing environment variables on app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,20 +8,12 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import appConfig from './config/app.config';
+import { DBConfig } from './config/db.config';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        host: process.env.DATABASE_HOST,
-        port: +process.env.DATABASE_PORT,
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+      useFactory: () => DBConfig,
     }),
     ConfigModule.forRoot({
       load: [appConfig],
