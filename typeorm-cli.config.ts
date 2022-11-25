c3126dce9e3c8f74b1c3a381a44bd91331d08502
Daniel Gomez <danielgomez@ibm.com>
2022-11-25 16:17:48 -0600

    using environment variables on typeorm-cli.config.ts

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -6,11 +6,11 @@ import { DataSource } from 'typeorm';
 
 export default new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'pass123',
-  database: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: [Todo, Tag],
   migrations: [TodoRefactor1660413916391, SchemaSync1660415276830],
 });
